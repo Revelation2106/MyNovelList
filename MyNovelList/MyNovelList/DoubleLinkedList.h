ad7535b13065748f92bf1a3c1ebdc52b408c463a
Revelation2106 <55440018+Revelation2106@users.noreply.github.com>
2021-05-14 20:47:17 +0100

    Added UWS plagiarism declaration to all .h files

--- MyNovelList/MyNovelList/DoubleLinkedList.h
+++ MyNovelList/MyNovelList/DoubleLinkedList.h
@@ -1,5 +1,17 @@
 #pragma once
 
+/*
+
+Name: Thomas Cole
+
+Student ID: B00269678
+
+I declare that the following code was produced by myself for the Algorithms & Collections - COMP09044 - module and that this is my own work.
+
+I am aware of the penalties incurred by submitting in full or in part work that is not my own and that was developed by third parties that are not appropriately acknowledged.
+
+*/
+
 #include <string>
 #include <vector>
 
